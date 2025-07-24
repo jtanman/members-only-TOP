1ac794ec788d8027060e74aca6e054a4229a1e9c
jtanman <jtanman@gmail.com>
2025-07-23 22:26:17 -0700

    Change port to use env port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const pool = require('./db');
 const bcrypt = require('bcrypt');
 
